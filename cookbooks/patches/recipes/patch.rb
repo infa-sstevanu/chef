886d6e95d922efe1396ac27146fb23a17563916e
stvnorg <stvn.org@gmail.com>
2020-11-19 18:15:27 -0600

    update the patch.rb file

--- cookbooks/patches/recipes/patch.rb
+++ cookbooks/patches/recipes/patch.rb
@@ -2,11 +2,19 @@ include_recipe 'base-config::setlocale'
 include_recipe 'base-pkg::install'
 
 node_hash = node.normal.to_hash
+
 detections = node_hash["HOST_LIST_VM_DETECTION_OUTPUT"]["RESPONSE"]["HOST_LIST"]["HOST"]["DETECTION_LIST"]["DETECTION"]
 severity_level = node_hash["qualys"]["severity_level"]
 
+# List of the packages that are need to be patched
 detections.each do |detection|
   if detection["SEVERITY"].to_i >= severity_level.to_i
-    log detection["RESULTS"]
+    packages = detection["RESULTS"].split("\n")
+    packages.each do |pkgs|
+      unless pkgs.include?("Installed")
+        pkg = pkgs.split(" ").select { |p| p.length>0 }
+        log "Update package #{pkg[0]} to version #{pkg[2]}"
+      end
+    end
   end
 end
