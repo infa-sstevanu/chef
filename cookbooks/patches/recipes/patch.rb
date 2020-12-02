fdd76efb811ad6a946dcca868aa228e5dcaadfe3
stvnorg <stvn.org@gmail.com>
2020-12-02 11:53:09 -0600

    bug fix and restructure the code

--- cookbooks/patches/recipes/patch.rb
+++ cookbooks/patches/recipes/patch.rb
@@ -1,54 +1,54 @@
-ignore_lines = [",", "Installed", "Vulnerable"]
+$ignore_lines = [",", "Installed", "Vulnerable"]
 
-vulnerable_packages = []
+$vulnerable_packages = []
 
-detections_list = node.normal.to_hash()["HOST_LIST_VM_DETECTION_OUTPUT"]["RESPONSE"]["HOST_LIST"]["HOST"]
+$detections_list = node.normal.to_hash()["HOST_LIST_VM_DETECTION_OUTPUT"]["RESPONSE"]["HOST_LIST"]["HOST"]
 
-# List of the packages that are needs to be patched
-begin
-  detections = detections_list["DETECTION_LIST"]["DETECTION"]
+# List of the packages required version that are need to be updated
+def get_packages_list(detections)
   detections.each do |detection|
     if detection["SEVERITY"].to_i >= node.default["qualys"]["severity_level"]
       packages = detection["RESULTS"].split("\n")
       packages.each do |pkgs|
-        unless (pkgs.length==0) || (pkgs.match? Regexp.union(ignore_lines))
+        unless (pkgs.length==0) || (pkgs.match? Regexp.union($ignore_lines))
           pkg = pkgs.split(" ").select { |p| p.length>0 }
-          vulnerable_packages.push([pkg[0], pkg[2]])
-        end
-      end
-    end
-  end  
-rescue
-  log(detections_list.length)
-  detections_list.each do |detections|
-    detections = detections["DETECTION_LIST"]["DETECTION"]
-    detections.each do |detection|
-      if detection["SEVERITY"].to_i >= node.default["qualys"]["severity_level"]
-        packages = detection["RESULTS"].split("\n")
-        packages.each do |pkgs|
-          unless (pkgs.length==0) || (pkgs.match? Regexp.union(ignore_lines))
-            pkg = pkgs.split(" ").select { |p| p.length>0 }
-            vulnerable_packages.push([pkg[0], pkg[2]])
-          end
+          pkg_name = pkg[0]
+          pkg_installed_version = pkg[1]
+          pkg_required_version = pkg[2]
+          $vulnerable_packages.push([pkg_name, pkg_required_version])
+          log("Package name: #{pkg_name}, Installed version: #{pkg_installed_version}, Required version: #{pkg_required_version}")
         end
       end
     end
   end
 end
 
+begin
+  $detections_list.each do |detections|
+    detections = detections["DETECTION_LIST"]["DETECTION"]
+    get_packages_list detections
+  end
+rescue
+  detections = $detections_list["DETECTION_LIST"]["DETECTION"]
+  get_packages_list detections
+end
+
 # Update the packages
-vulnerable_packages.each do |pkg|
+$vulnerable_packages.each do |pkg|
   begin
     pkg_name = pkg[0]
-    pkg_version = pkg[2]
-    
-    log(pkg)
+    pkg_required_version = pkg[1]
+
+    log("Installing #{pkg_name} version #{pkg_required_version}")
+
+    <<-DOC
     yum_package pkg_name do
       flush_cache [:before]
       package_name pkg_name
-      version pkg_version
+      version pkg_required_version
       action :install
     end
+    DOC
   rescue
   end
 end
